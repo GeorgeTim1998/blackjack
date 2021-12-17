f9afadb9b9bf4520338c52573f2b9be174713442
georgetim1998 <georgetim1998@gmail.com>
2021-12-17 21:07:59 +0300

    cards generate

--- cards.rb
+++ cards.rb
@@ -1,12 +1,16 @@
-class ClassName
+class Cards
   CARDS = %w[T 2 3 4 5 6 7 8 9 10 J Q K].freeze
+  CARDS_AMOUNT = 13
   CARD_SUITS = %w[<3 <> + ^].freeze
+  CARD_SUITS_AMOUNT = 4
+
+  attr_writer :card
 
   def initialize
-    generate_cards
+    @card = designate
   end
 
-  def generate_cards
-    
+  def designate
+    CARDS[rand(CARDS_AMOUNT)].concat(CARD_SUITS[rand(CARD_SUITS_AMOUNT)])
   end
 end
