8abc9c412a1f3f63926e72c7822ce92c0d1896e2
georgetim1998 <georgetim1998@gmail.com>
2021-12-17 20:55:51 +0300

    starting  generate cards methode

---
+++ cards.rb
@@ -0,0 +1,12 @@
+class ClassName
+  CARDS = %w[T 2 3 4 5 6 7 8 9 10 J Q K].freeze
+  CARD_SUITS = %w[<3 <> + ^].freeze
+
+  def initialize
+    generate_cards
+  end
+
+  def generate_cards
+    
+  end
+end
