01c63297a697d8cf300429a589ad9b238e36421b
georgetim1998 <georgetim1998@gmail.com>
2021-12-19 21:55:03 +0300

    almost finished

--- game.rb
+++ game.rb
@@ -3,8 +3,8 @@ class Blackjack
   attr_reader :player, :dealer, :player_points, :dealer_points
 
   COUNT_POINTS = { 'T': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-                   'J': 10, 'Q': 10, 'K': 10 }.freeze 
-  WINING_POINTS = 21.freeze
+                   'J': 10, 'Q': 10, 'K': 10 }.freeze
+  WINING_POINTS = 21
 
   def initialize(name)
     @player = Player.new(name)
@@ -34,35 +34,36 @@ class Blackjack
 
   def value_cards(player)
     aces = how_many_aces? player.cards
-
     case aces
     when 0
       value_cards_no_ace(player)
-    when 1
-      value_cards_one_ace(player)
-    when 2
-      value_cards_2_aces(player)
+    when 1 || 2
+      value_1_or_2_aces(player)
     else
       value_cards_3_aces
     end
   end
 
-  def value_cards_1_ace(player)
-    value_no_ace = value_cards_no_ace(player)
-    value_ace = value_no_ace + 10
-
-    if value_ace > WINING_POINTS
-      value_no_ace
-    else
-      value_ace
-    end
-  end
-
   def value_cards_2_aces(player)
-    
+    if player.cards.length == 2
+      11 + 1
+    else
+      two_aces_3_cards_if(player)
+    end
   end
 
   def value_cards_3_aces
     11 + 1 + 1
+  end
+
+  def value_1_or_2_aces(player)
+    min_value = value_cards_no_ace(player)
+    max_value = min_value + 10
+
+    if max_value > WINING_POINTS
+      min_value
+    else
+      max_value
+    end
   end
 end
